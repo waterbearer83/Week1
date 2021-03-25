8a12d85559b8a0967836ddfbdc37dcb3d889c382
waterbearer83 <80931874+waterbearer83@users.noreply.github.com>
2021-03-24 22:36:43 -0500

    first commit

---
+++ App.java
@@ -0,0 +1,20 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//week 1 coding assignment
+		
+		//Question 3
+		double itemPrice = 10.00;
+		double moneyInWallet = 250.00;
+		
+		//Question 4
+		double walletAfterPurchase = moneyInWallet - itemPrice;
+		
+		//Question 5
+		System.out.println("I now have $" + walletAfterPurchase + " after buying that book.");
+		
+	}
+
+}
